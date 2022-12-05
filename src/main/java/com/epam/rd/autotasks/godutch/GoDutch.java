c845a6abdaa92bc61bfe7a7953ef406a81d2a0be
firdavs <firdavsolimjonov25@gmail.com>
2022-12-05 12:58:18 +0500

    add

--- src/main/java/com/epam/rd/autotasks/godutch/GoDutch.java
+++ src/main/java/com/epam/rd/autotasks/godutch/GoDutch.java
@@ -1,8 +1,26 @@
 package com.epam.rd.autotasks.godutch;
 
+import java.util.Scanner;
+
 public class GoDutch {
 
     public static void main(String[] args) {
         //Write code here
+        Scanner scanner = new Scanner(System.in);
+
+        int bill = scanner.nextInt();
+        if (bill<0){
+            System.out.println("Bill total amount cannot be negative");
+            return;
+        }
+
+        int numberOfFriends = scanner.nextInt();
+        if (numberOfFriends<=0) {
+            System.out.println("Number of friends cannot be negative or zero");
+            return;
+        }
+
+        bill += bill/10;
+        System.out.println(bill/numberOfFriends);
     }
 }
